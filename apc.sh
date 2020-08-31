a8eabe52530d114a15f13b37cc4b1b745a653f1d
Jeffrie <Jeffrie@callbox.com>
2020-08-31 13:49:54 -0500

    new files

---
+++ apc.sh
@@ -0,0 +1,10 @@
+curl --location --request POST 'https://api.atmzphr.dev/?add' \
+--header 'X-API-atmzphr-Key-x123-auth: thesuck-inoyrcice-iwyuvay-svikiypf-289ds' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+ "email": "someemail@email.com",
+ "lan_ip": "123.123.123.123",
+ "wan_ip": "123.123.123.123",
+ "pub_key": "jklasdfj;asdfASdfAsdfjklasdfjkl;adfdf=",
+ "end_point": "10.12.12.1:83484"
+}'
